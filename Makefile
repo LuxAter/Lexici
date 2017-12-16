8ef9dcd1837b3939400687b51fc85d8e4e42243b
Arden Rasmussen <ardenisthebest@gmail.com>
2017-12-16 15:41:20 -0800

    Ada p4

--- Makefile
+++ Makefile
@@ -16,12 +16,14 @@ all:
 	$(call submake,p1)
 	$(call submake,p2)
 	$(call submake,p3)
+	$(call submake,p4)
 	printf "%b\n" "\033[0;34mCompiled all problems\033[0m"
 
 clean:
 	$(call submake,p1,clean)
 	$(call submake,p2,clean)
 	$(call submake,p3,clean)
+	$(call submake,p4,clean)
 	printf "%b\n" "\033[0;34mCleaned all problems\033[0m"
 
 
---
+++ p4/p4.adb
@@ -0,0 +1,56 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+
+procedure P4 is
+  function ReverseInteger( n:Integer ) return Integer is
+    reversed:Integer := 0;
+    val:Integer := n;
+  begin
+    while val > 0 loop
+      reversed := (10 * reversed) + (val mod 10);
+      val := val / 10;
+    end loop;
+    return reversed;
+  end ReverseInteger;
+  function IsPalindrome ( n:Integer ) return Boolean is
+  begin
+    return n = ReverseInteger(n);
+  end IsPalindrome;
+  a:Integer := 999;
+  b:Integer := 0;
+  db:Integer := 0;
+  max:Integer := 999;
+  count:Integer := 3;
+  largest:Integer := 0;
+begin
+  if Argument_Count >= 1 then
+    count := Integer'Value(Argument(1));
+    while count > 0 loop
+      max := (max * 10) + 9;
+      count := count - 1;
+    end loop;
+    a := max;
+  end if;
+  while a >= 100 loop
+    if a mod 11 = 0 then
+      b := max;
+      db := 1;
+    else
+      b := 11 * (max / 11);
+      db := 11;
+    end if;
+    while b >= a loop
+      if a * b <= largest then
+        exit;
+      end if;
+      if IsPalindrome( a * b ) = True then
+        largest := a * b;
+      end if;
+      b := b - db;
+    end loop;
+    a := a - 1;
+  end loop;
+  Put_Line(Integer'Image(largest));
+end P4;
+
