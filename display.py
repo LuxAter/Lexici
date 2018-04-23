ddf01ae18d3748b8d5bda9bbd15b0d588bdf71e4
Arden Rasmussen <ardenisthebest@gmail.com>
2018-04-22 18:13:38 -0700

    Added run script

---
+++ display.py
@@ -0,0 +1,163 @@
+import math
+import os
+from pprint import pprint
+
+
+def get_json(obj, key):
+    if isinstance(obj, dict):
+        if key.split('.')[0] in obj:
+            if key.split('.')[1:] != list():
+                return get_json(obj[key.split('.')[0]], '.'.join(
+                    key.split('.')[1:]))
+            return obj[key.split('.')[0]]
+    return None
+
+
+def set_color(color):
+    color = tuple(int(color[i:i + 2], 16) for i in (1, 3, 5))
+    return "\033[38;2;{};{};{}m".format(color[0], color[1], color[2])
+
+
+def display_table(data, gap=2, width=40, height=10, axis=True):
+    max_height, max_width = os.popen('stty size', 'r').read().split()
+    if width == 'max':
+        width = int(max_width)
+    if height == 'max':
+        height = int(max_height)
+    cols = {}
+    for entry in data:
+        for key, value in entry.items():
+            if isinstance(value, float):
+                value = float("{:.3}".format(value))
+            if key in cols:
+                cols[key].append(value)
+            else:
+                cols[key] = [value]
+    widths = [
+        len(str(max(x, key=lambda st: len(str(st))))) for _, x in cols.items()
+    ]
+    for i, key in enumerate(cols):
+        if key == 'color':
+            widths[i] = 3
+        else:
+            widths[i] = max(widths[i], len(key))
+    for i in range(len(data) + axis):
+        if i == 0:
+            for j, kv in enumerate(cols.items()):
+                key = kv[0]
+                if key == 'color':
+                    print('{}'.format(' ' * (widths[j] + gap)), end='')
+                else:
+                    print(
+                        "\033[1m{:{}}\033[0m{}".format(key.title(), widths[j],
+                                                       ' ' * gap),
+                        end='')
+            print()
+            print("─" * (sum(widths) + (gap * len(widths))))
+        else:
+            if i % 2 == 0:
+                print('\033[40m', end='')
+            for j, kv in enumerate(cols.items()):
+                key = kv[0]
+                value = kv[1]
+                if key == 'color':
+                    print(
+                        "{}{:>{}}{}{}".format(
+                            set_color(value[i - axis]), '', widths[j],
+                            '\033[39m', ' ' * gap),
+                        end='')
+                else:
+                    print(
+                        "{:<{}}{}".format(value[i - axis], widths[j],
+                                          ' ' * gap),
+                        end='')
+            if i % 2 == 0:
+                print('\033[0m', end='')
+            print()
+
+
+def display_graph(data, gap=2, width=40, height=10, axis=True):
+    max_height, max_width = os.popen('stty size', 'r').read().split()
+    if width == 'max':
+        width = int(max_width)
+    if height == 'max':
+        height = int(max_height)
+    if axis is True:
+        height -= 1
+    normalizer = max(data, key=lambda item: item[0])[0]
+    for i, entry in enumerate(data):
+        if not (isinstance(entry, tuple) or isinstance(entry, list)):
+            data[i] = (height * entry / normalizer, '#ffffff')
+        else:
+            data[i] = (height * entry[0] / normalizer, entry[1])
+    if isinstance(normalizer, int):
+        axis_width = len("{}".format(normalizer)) + 1
+    else:
+        axis_width = len("{:.3}".format(normalizer)) + 1
+    width -= (axis_width + 1)
+    bar_width = int(width / len(data)) - gap
+    for i in range(height):
+        if axis is True:
+            if i == int(height / 4) or i == int(height / 2) or i == int(
+                    3 * height / 4) or i == 0 or i == height:
+                if isinstance(normalizer, int):
+                    print(
+                        "{:{}}│".format(
+                            int(normalizer * (height - i) / height),
+                            axis_width),
+                        end='')
+                else:
+                    print(
+                        "{:{}.3}│".format(normalizer * (height - i) / height,
+                                          axis_width),
+                        end='')
+            else:
+                print("{}│".format(" " * axis_width), end='')
+        for val in data:
+            char = '█'
+            if height - i <= val[0]:
+                char = '█'
+            elif height - i - 1 <= val[0]:
+                perc = int((val[0] % 1) * 8)
+                if perc == 0:
+                    char = ' '
+                elif perc == 1:
+                    char = '▁'
+                elif perc == 2:
+                    char = '▂'
+                elif perc == 3:
+                    char = '▃'
+                elif perc == 4:
+                    char = '▄'
+                elif perc == 5:
+                    char = '▅'
+                elif perc == 6:
+                    char = '▆'
+                elif perc == 7:
+                    char = '▇'
+            else:
+                char = ' '
+            print(
+                "{}{}{}{}".format(
+                    set_color(val[1]), char * bar_width, "\033[0m", " " * gap),
+                end='')
+        print()
+    if axis is True:
+        print("{}└{}".format(' ' * axis_width,
+                             '─' * ((bar_width + gap) * len(data))))
+
+
+def format_graph_data(data, comp):
+    values = []
+    for key, value in data.items():
+        values.append((get_json(value, comp), value['color']))
+    return values
+
+
+def format_table_data(data, values):
+    res = []
+    for key, value in data.items():
+        res.append(dict())
+        for val in values:
+            res[-1][val.split('.')[-1]] = get_json(value, val)
+    return res
---
+++ run.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3
+import argparse
+import glob
+import os
+import time
+import subprocess
+import sys
+import json
+from pprint import pprint
+from enum import Enum
+
+from display import *
+
+language_colors = dict()
+
+class Capture(Enum):
+    TIME = 0
+    LOC = 1
+
+def display_name(name):
+    names = {'cpp': 'C++', 'python3': 'Python'}
+    if name in names:
+        return names[name].title()
+    return name.title()
+
+def display_color(lang):
+    global language_colors
+    if language_colors == dict():
+        tmp = json.load(open('languages.json', 'r'))
+        for key, value in tmp.items():
+            language_colors[key] = value['color']
+    if lang in language_colors:
+        return language_colors[lang]
+    return 'aaafb4'
+
+def title(string, color, size=40):
+    print("\033[{};1m{}\n{:^{}}\n{}\033[0m".format(color, '=' * size, string, size, '=' * size))
+
+def subtitle(string, color, size=40):
+    print("\033[{};1;4m{:^{}}\033[0m".format(color, string, size))
+
+def par(string, size=40):
+    print("\033[1;4m{:^{}}\033[0m".format(string, size))
+
+
+def get_exes(names):
+    files = glob.glob('*/{}.*'.format(names))
+    exes = []
+    for file in files:
+        if os.path.isfile(os.path.join(os.getcwd(), file)) and os.access(
+                os.path.join(os.getcwd(), file), os.X_OK):
+            exes.append(file)
+    return exes, files
+
+def run_exe(exe, reps, longest=0):
+    avg = 0
+    spacing = longest - 8 - (2 * len(str(reps)))
+    min_t = 1e20
+    max_t = 0
+    print("\033[33m[ ] {:{}} [0/{}]\033[0m".format(exe,spacing, reps), end='')
+    sys.stdout.flush()
+    for i in range(reps):
+        print("\033[G\033[33m[ ] {:{}} [{}/{}]\033[0m".format(exe, spacing, i, reps), end='')
+        sys.stdout.flush()
+        start = time.time()
+        result = subprocess.run([exe], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        end = time.time()
+        diff = end - start
+        max_t = max(max_t, diff)
+        min_t = min(min_t, diff)
+        avg += diff
+    print("\033[G\033[32m[] {:{}} [{}/{}]\033[0m".format(exe, spacing, reps, reps))
+    return min_t, (avg / reps), max_t
+
+def run(exes, files, reps, capture=[Capture.TIME, Capture.LOC]):
+    length = max(len(max(exes, key=len)) + 6 + (2 * len(str(reps))), 40)
+    subtitle('Collecting Data', 35, length)
+    data = {}
+    for exe in exes:
+        language = os.path.dirname(exe)
+        data[language] = dict()
+        data[language]['name'] = display_name(language)
+        data[language]['color'] = display_color(data[language]['name'])
+        if Capture.TIME in capture:
+            min_t, avg_t, max_t = run_exe(exe, reps, length)
+            data[language]['time'] = dict()
+            data[language]['time']['min'] = min_t
+            data[language]['time']['avg'] = avg_t
+            data[language]['time']['max'] = max_t
+        if Capture.LOC in capture:
+            data[language]['size'] = dict()
+            if exe.endswith('a'):
+                matching = [s for s in files if exe[:-2] in s]
+                for match in matching:
+                    if not match.endswith('.a'):
+                        file = match
+            else:
+                file = exe
+            data[language]['size']['bytes'] = os.path.getsize(file)
+            lines = 0
+            with open(file, 'r') as f:
+                lines = len(f.readlines())
+            data[language]['size']['lines'] = lines
+        if Capture.LOC in capture and Capture.TIME in capture:
+            data[language]['eff'] = data[language]['size']['bytes'] * data[language]['time']['avg']
+    return data
+
+def display(data, args):
+    subtitle('Results', 35)
+    if 'avg' in args.graph:
+        par('Average Time')
+        display_graph(format_graph_data(data, 'time.avg'))
+    if 'min' in args.graph:
+        par('Minimum Time')
+        display_graph(format_graph_data(data, 'time.min'))
+    if 'max' in args.graph:
+        par('Maximum Time')
+        display_graph(format_graph_data(data, 'time.max'))
+    if 'eff' in args.graph:
+        par("Efficiency")
+        display_graph(format_graph_data(data, 'eff'))
+    if 'bytes' in args.graph:
+        par('Bytes')
+        display_graph(format_graph_data(data, 'size.bytes'))
+    if 'lines' in args.graph:
+        par('Lines')
+        display_graph(format_graph_data(data, 'size.lines'))
+    table_data = args.table
+    for i, value in enumerate(table_data):
+        if value == 'avg':
+            table_data[i] = 'time.avg'
+        elif value == 'min':
+            table_data[i] = 'time.min'
+        elif value == 'max':
+            table_data[i] = 'time.max'
+        elif value == 'bytes':
+            table_data[i] = 'size.bytes'
+        elif value == 'lines':
+            table_data[i] = 'size.lines'
+    display_table(format_table_data(data, table_data))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'exe', type=str, help='executable name to run in each directory.')
+    parser.add_argument(
+        '-r',
+        '--reps',
+        type=int,
+        default=5,
+        help='number of times to run each executable.')
+    parser.add_argument(
+        '--args',
+        nargs='*',
+        help='arguments to pass to the executables.')
+
+    parser.add_argument(
+        '-c', '--color', action='store_true', help='enables color in output.')
+    parser.add_argument(
+            '-g', '--graph', default=['bytes'], nargs='*', help='data sets to graph', choices={'min', 'max', 'avg', 'eff', 'bytes', 'lines'}
+            )
+    parser.add_argument(
+            '-t', '--table', default=['color', 'name', 'avg', 'bytes', 'eff'], nargs='*', help='data sets to place in table', choices={'color', 'name', 'min', 'max', 'avg', 'eff', 'bytes', 'lines'}
+            )
+    args = parser.parse_args()
+    title('RUNNING', 35)
+    exes, files = get_exes(args.exe)
+    data = run(exes, files, args.reps)
+    # subtitle('Time', 35)
+    # display_graph(format_graph_data(data, 'time.avg'))
+    # subtitle('Efficiency', 35)
+    # display_graph(format_graph_data(data, 'eff'), 2, 80, 40)
+    # subtitle('Bytes of Code', 35)
+    # display_graph(format_graph_data(data, 'size.bytes'), 2, 'max', 'max')
+    # display_table(format_table_data(data, ['color', 'name', 'time.min', 'time.avg', 'time.max', 'size.bytes', 'eff']))
+    display(data, args)
+
+if __name__ == "__main__":
+    main()
